f61ade13dcc6acc15ca62db173d4ab39a51b88e7
MoonSky1! <vanmathyrr@gmail.com>
2024-03-12 11:43:02 -0400

    Initial Commit

---
+++ src/webparts/spfxMsgraphResting/components/ISpfxMsgraphRestingProps.ts
@@ -0,0 +1,7 @@
+export interface ISpfxMsgraphRestingProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/spfxMsgraphResting/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ISpfxMsgraphRestingWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'SpfxMsgraphRestingWebPartStrings' {
+  const strings: ISpfxMsgraphRestingWebPartStrings;
+  export = strings;
+}
